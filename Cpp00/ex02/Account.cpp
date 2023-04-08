eb8409188c1ff69318ddb1b4acd57da677dae1da
Esra <71944354+EsraAblak@users.noreply.github.com>
2023-04-08 15:35:32 +0300

    const-acc ok

--- Cpp00/ex02/Account.cpp
+++ Cpp00/ex02/Account.cpp
@@ -1,4 +1,6 @@
 #include "Account.hpp"
+#include <iomanip>
+// #include <ctime>
 #include <iostream>
 
 int Account::_nbAccounts = 0;
@@ -8,8 +10,7 @@ int Account::_totalNbWithdrawals = 0;
 
 int Account::getNbAccounts()
 {
-	return (_nbAccounts); //static olduğu için böyle ulaşbiliyosun!!
-							//yukarıda Account yine tanımlı olmak zorunda tekrarb ak
+	return (_nbAccounts); // this ?
 }
 
 int Account::getTotalAmount()
@@ -27,7 +28,12 @@ int Account::getNbWithdrawals()
 	return (_totalNbWithdrawals);
 }
 
-int Account::checkAmount() const
+int Account::checkAmount() const /*
+Burada, checkAmount() fonksiyonunun const olarak işaretlenmesi,
+	fonksiyonun sınıfın durumunu değiştirmeyeceği ve nesnenin veri üyelerini değiştirmeyeceği anlamına gelir. Yani,
+	checkAmount() fonksiyonu nesne durumunu okumaya yönelik bir işlem yapacak,
+	ancak nesneyi değiştirmeyecek. Bu,
+	fonksiyonun const-correctness prensibine uygun olarak tanımlandığını gösterir.*/
 {
 	return (this->_amount);
 }
@@ -39,9 +45,27 @@ Account::Account(int initial_deposit)
 	this->_amount = initial_deposit;
 	this->_nbDeposits = 0;
 	this->_nbWithdrawals = 0;
-	//this->_displayTimestamp();
+	_displayTimestamp();
 	std::cout << "index: " << this->_accountIndex
-			<< ";"	<< "amount: " << this->checkAmount()
-			<< ";"	<< "created" << std::endl;
+				<< ";"
+				<< "amount: " << this->checkAmount()
+				<< ";"
+				<< "created" << std::endl;
 	this->_totalAmount += initial_deposit;
+}
+
+void Account::_displayTimestamp()
+{
+	std::time_t t = std::time(NULL);
+	std::tm now = *localtime(&t);
+	std::cout
+		<< "["
+		<< (now.tm_year + 1900)
+		<< std::setfill('0')
+		<< std::setw(2) << now.tm_mon + 1
+		<< std::setw(2) << now.tm_mday << "_"
+		<< std::setw(2) << now.tm_hour
+		<< std::setw(2) << now.tm_min
+		<< std::setw(2) << now.tm_sec
+		<< "] ";
 }
