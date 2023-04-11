270a059ce2e09fd1e7062daa9f4457a5d8082763
Esra <71944354+EsraAblak@users.noreply.github.com>
2023-04-11 16:56:09 +0300

    stack-heap

--- Cpp01/ex00/main.cpp
+++ Cpp01/ex00/main.cpp
@@ -9,3 +9,15 @@ int	main(void)
 	randomChump("zombie3");
 	delete zombie2;
 }
+
+/*
+	Stack | Heap
+Stack -> static hafızanın tahsisi (değişmeyen örn; referansın doğrudan değişimi)
+Heap ->dinamik hafıza tahsisi
+
+S -> değişkenlere doğrudan erişilir  -> bu yüzden daha hızlı erişim
+H -> değişkenlere dolaylı yoldan erişim (örn; i1.isim)
+
+S -> program complie-time bellek tahsisi gerçekleşir. (derlenme esnasında)
+H -> program runtime esnasında bellek tahsisi. (çalışma esnasında)
+*/
