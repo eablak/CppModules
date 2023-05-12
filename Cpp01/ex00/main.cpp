5836f5bb96e922af01de6ff6cd8b00d89427e01c
esra ablak <esra@kali.kali>
2023-05-12 15:15:56 +0300

    okey

--- Cpp01/ex00/main.cpp
+++ Cpp01/ex00/main.cpp
@@ -2,10 +2,12 @@
 
 int	main(void)
 {
+	
 	Zombie zombie("Zombie1");
 	Zombie *zombie2 = newZombie("zombie2");
 	zombie.announce();
 	zombie2->announce();
 	randomChump("zombie3");
 	delete zombie2;
+
 }
