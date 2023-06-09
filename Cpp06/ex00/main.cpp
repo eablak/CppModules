d512f855a92611b835a813600003b1255fb2c511
EsraAblak <esrablk9@gmail.com>
2023-06-09 19:15:17 +0300

    wrong

--- Cpp06/ex00/main.cpp
+++ Cpp06/ex00/main.cpp
@@ -1,8 +1,42 @@
 #include "ScalarConverter.hpp"
+#include <iomanip>
+
+void result(ScalarConverter *s, int type)
+{
+    float number = atof(&(s->getString())[0]);
+    std::cout << "number " << number << std::endl;
+    std::cout << "char: ";
+    if (type == 0)
+        std::cout << "Non displayable" << std::endl;
+    else
+        std::cout << "'" <<  static_cast<char>(number) << "'" << std::endl;
+    std::cout << "int: " << static_cast<int>(number) << std::endl;
+    std::cout << std::fixed << std::scientific << std::setprecision(3) << "float: " << static_cast<float>(number) << "f" << std::endl;
+    std::cout << "double: " << static_cast<double>(number) << std::endl;
+}
+
+void _convert(ScalarConverter *s)
+{
+    switch(s->getType())
+    {
+        case 0:
+            result(s, 0);
+            break;
+        case 1:
+            result(s, 1);
+            break;
+        case 2:
+            result(s, 2);
+            break;
+        case 3:
+            result(s, 3);
+            break;
+    }
+}
 
 void check_unknown(ScalarConverter *s)
 {
-    std::cout << s->getString() << " is not convertable!" << std::endl;
+    std::cout << "'" << s->getString() << "' is not convertable!" << std::endl;
     exit(1);
 }
 
@@ -16,5 +50,5 @@ int main(int ac, char **av)
     s.findType(&s);
     if (s.getType() == 4)
         check_unknown(&s);
-    std::cout << "type " << s.getType() << std::endl;
+    _convert(&s);
 }
