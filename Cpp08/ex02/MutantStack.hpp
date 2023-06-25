3712428913547520c2825cdd2781e8f0ef8b7832
EsraAblak <esrablk9@gmail.com>
2023-06-25 02:45:41 -0400

    ok

---
+++ Cpp08/ex02/MutantStack.hpp
@@ -0,0 +1,24 @@
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <iostream>
+#include <stack>
+
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+    public:
+    MutantStack(){};
+    MutantStack(const MutantStack &copyM){ *this = copyM; }
+    MutantStack &operator=(const MutantStack &copyM) { return *this; }
+    ~MutantStack(){};
+
+    typedef typename std::stack<T>::container_type::iterator iterator;
+
+    iterator begin() {return this->c.begin(); }
+    iterator end() {return this->c.end(); }
+};
+
+
+#endif
